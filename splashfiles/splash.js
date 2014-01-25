79ccd84bd0e0a3c273c1ef3ed0ecbb2d4a07fe5c
seaside98 <add98@icloud.com>
2014-01-25 09:38:43 -0600

    Adding login
    Login should work, but the PHP hasn't been implemented yet. I am also looking to move the circles to PHP. I'm not sure what to do on mobile with search or login.

--- splashfiles/splash.js
+++ splashfiles/splash.js
@@ -1,63 +1,35 @@
-var suggestions = {};
 var ajaxPool = [];
 var listWikis = ['en', 'customs', 'cuusoo', 'stories', 'meta'];
+var articles = 0;
+var ii = 0;
+var activeCover = 0;
 
 $(document).ready(function() {
-	
-	$.ajax({
-		type: "GET",
-		url: "http://meta.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&format=json",
-		dataType: "jsonp",
-		success: function(data) {
-			accounts = data.query.statistics.users;
-			//if (accounts >= 1000) {
-				accounts = ((accounts/1000).toFixed(1).replace(/\.?0+$/, "")) + "k";
-			//}
-			$("#circle-accounts .circle-number").append(accounts);
-			$.ajax({
-				type: "GET",
-				url: "http://en.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&format=json",
-				dataType: "jsonp",
-				success: function(data) {
-					articles = parseInt(data.query.statistics.articles, 10);
-					$.ajax({
-						type: "GET",
-						url: "http://customs.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&format=json",
-						dataType: "jsonp",
-						success: function(data) {
-							articles += parseInt(data.query.statistics.articles, 10);
-							$.ajax({
-								type: "GET",
-								url: "http://stories.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&format=json",
-								dataType: "jsonp",
-								success: function(data) {
-									articles += parseInt(data.query.statistics.articles, 10);
-									$.ajax({
-										type: "GET",
-										url: "http://cuusoo.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&format=json",
-										dataType: "jsonp",
-										success: function(data) {
-											articles += parseInt(data.query.statistics.articles, 10);
-											//if (articles >= 10000) {
-												articles = ((articles/1000).toFixed()) + "k";
-											//}
-											$("#circle-articles .circle-number").append(articles);
-											$("#circles").css({"display": "block"});
-										} //cuusoo success
-									}); //cuusoo ajax
-								} //stories success
-							}); //stories ajax
-						} //customs success
-					}); //customs ajax		
-				} //en success
-			}); //en ajax
+	$.get('http://meta.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&origin=http%3A%2F%2F' + document.domain + '&format=json',
+		function(data) {
+			var accounts = data.query.statistics.users;
+			accounts = ((accounts/1000).toFixed(1).replace(/\.?0+$/, "")) + "k";
+			$('#circle-accounts .circle-number').text(accounts);
+			for (var i = 0; i < listWikis.length - 1; i++) {
+				$.get('http://' + listWikis[i] + '.brickimedia.org/api.php?action=query&meta=siteinfo&siprop=statistics&origin=http%3A%2F%2F' + document.domain + '&format=json',
+					function(data) {
+						articles += parseInt(data.query.statistics.articles, 10);
+						ii++; //total successes
+						if (ii == listWikis.length - 1) {
+							articles = ((articles/1000).toFixed()) + 'k';
+							$('#circle-articles .circle-number').text(articles);
+							$('#circles').show();
+						} //last success
+					} //success
+				); //ajax
+			} //not meta
 		} //meta success
-	}); //meta ajax
+	); //meta ajax
 	
 	$(window).scroll(function() {
 		var diffToTop = $(this).scrollTop();
 		var diffToBottom = $(document).height() - ($(this).scrollTop() + $(this).height());
-		if (window.innerWidth > 780) {
+		if (window.innerWidth > 780) { //mobile or tablet
 			$("#hero").css("background-position", "center " + parseInt(-diffToTop / 5, 10) + "px");
 			$("#blue-container").css("background-position", "center " + parseInt(-diffToTop / 5, 10) + "px");
 			$("#red-container").css("background-position", "center " + (150 + parseInt(-diffToTop / 5, 10)) + "px");
@@ -68,7 +40,7 @@ $(document).ready(function() {
 			if (diffToBottom <= 100) {
 				$("#wiki-nav-bottom").css("opacity", (100 - diffToBottom) / 100);
 			}
-		} else {
+		} else { //desktop
 			$("#hero").css("background-position", "center 0");
 			$("#blue-container").css("background-position", "center 0");
 			$("#red-container").css("background-position", "center 0");
@@ -86,7 +58,7 @@ $(document).ready(function() {
 		} else {
 			$('#searchInput').show().css('width', 200).attr('placeholder', 'Search Brickimedia');
 		}
-	});
+	}); //resize search
 	
 	$('#searchInput').keyup(function() {
 		for (var i in ajaxPool) {
@@ -96,18 +68,17 @@ $(document).ready(function() {
 		var uriText = encodeURIComponent(searchText);
 		$('#searchSuggestions').html('');
 		for (var i = 0; i < listWikis.length; i++) {
-			ajaxPool[i] = $.ajax({
-				type: "GET",
-				url: "http://" + listWikis[i] + ".brickimedia.org/api.php?action=opensearch&search=" + uriText + "&limit=3&namespace=0&format=json",
-				dataType: "jsonp",
-				success: function(data) {
+			ajaxPool[i] = $.get('http://' + listWikis[i] + '.brickimedia.org/api.php?action=opensearch&search=' + uriText + '&limit=3&namespace=0&origin=http%3A%2F%2F' + document.domain + '&format=json',
+				function(data) {
 					addSearchResults(data[1], this.url);
-				}
-			}); //search ajax
+				} //success
+			); //ajax
 		}
 	}); //on search change
 	
 	$('#searchInput').click(function() {
+		$('#clearOverlay').css('right', '-24px');
+		$('#loginForm').css({'pointer-events': 'none', 'opacity': 0});
 		$('#searchClear').css('right', '17px');
 		$('#searchResults').css({'pointer-events': 'auto', 'opacity': 1});
 		$('html, body').animate({ scrollTop: 0 }, 500).css('overflow-y', 'hidden');
@@ -121,6 +92,82 @@ $(document).ready(function() {
 	}); //hide search
 	
 	$(window).resize();
+	
+	$('#loginSubmit').click(function() {
+		if ( !$('#loginUsername').val() ) {
+			$('#loginUsername').css('background-color', '#f00');
+			setTimeout(function() { $('#loginUsername').css('background-color', '#fff'); }, 2000);
+		}
+		if ( !$('#loginPassword').val() ) {
+			$('#loginPassword').css('background-color', '#f00');
+			setTimeout(function() { $('#loginPassword').css('background-color', '#fff'); }, 2000);
+		} else if ( $('#loginUsername').val() ) {
+			var username = encodeURIComponent( $('#loginUsername').val() );
+			var password = encodeURIComponent( $('#loginPassword').val() );
+			$.ajax({
+				'type': 'POST',
+				'url': 'http://meta.brickimedia.org?action=login&lgusername=' + username + '&lgpassword=' + password + '&origin=http%3A%2F%2F' + document.domain + '&format=json',
+				'cache': false,
+				'xhrFields': {
+					'withCredentials': true
+				},
+				'success': function(data) {
+					if (data.login.result == 'NeedToken') {
+						$.ajax({
+							'type': 'POST',
+							'url': 'http://meta.brickimedia.org?action=login&lgusername=' + username + '&lgpassword=' + password + '&lgtoken=' + data.login.token + '&origin=http%3A%2F%2F' + document.domain + '&format=json',
+							'cache': false,
+							'xhrFields': {
+								'withCredentials': true
+							},
+							'success': function() {
+								location.reload();
+							}
+						});
+					} else if (data.login.result == 'Blocked') {
+						showError('Your account is blocked');
+					} else {
+						showError('Username or password is incorrect');
+					}
+				},
+				'error': function() {
+					showError('Login failed because of an error');
+				}
+			});
+		}
+	});
+	
+	$('.loginOpen').click(function() {
+		$('#searchClear').css('right', '-24px');
+		$('#searchResults').css({'pointer-events': 'none', 'opacity': 0});
+		$('#clearOverlay').css('right', '17px');
+		$('#loginForm').css({'pointer-events': 'auto', 'opacity': 1});
+		$('html, body').animate({ scrollTop: 0 }, 500).css('overflow-y', 'hidden');
+		$('#loginUsername').select();
+	}); //show login
+	
+	$('#clearOverlay').click(function() {
+		$('#clearOverlay').css('right', '-24px');
+		$('#loginForm').css({'pointer-events': 'none', 'opacity': 0});
+		$('html, body').css('overflow-y', 'auto');
+	}); //hide login
+	
+	$('#loginUsername').keyup(function(e) {
+		if (e.keyCode == 13) {
+			$('#loginPassword').select();
+		}
+	});
+	$('#loginPassword').keydown(function(e) {
+		if (e.keyCode == 13) {
+			$('#loginSubmit').css('background-color', '#fff');
+		}
+	});
+	$('#loginPassword').keyup(function(e) {
+		if (e.keyCode == 13) {
+			$('#loginSubmit').click();
+			$('#loginSubmit').css('background-color', '#aaa');
+		}
+	});
 });
 
 function addSearchResults(results, name) {
@@ -135,3 +182,12 @@ function addSearchResults(results, name) {
 		$('#' + name).hide();
 	}
 } //add search results
+
+function showError(text) {
+	$('#loginError').text(text).css({'pointer-events': 'auto', 'opacity': 1});
+	$('#loginSubmit').css('background-color', '#f00');
+	setTimeout(function() { 
+		$('#loginError').css({'pointer-events': 'none', 'opacity': 0}); 
+		$('#loginSubmit').css('background-color', '#aaa'); 
+	}, 3000);
+}
