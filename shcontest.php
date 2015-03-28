9528ba4cf59747cb166abd2eb0fde440923a3380
George Barnick <georgebarnick@gmail.com>
2015-03-27 23:45:16 -0400

    If this doesn't work I'll lose it

--- shcontest.php
+++ shcontest.php
@@ -2,10 +2,7 @@
 <body>
 <?php
 $username = $_COOKIE["sharedUserName"];
-$url = "http://en.brickimedia.org/index.php?title=Brickipedia:Pick_the_Hero_contest/entries/" . $_COOKIE["sharedUserName"] . "&action=edit&preload=MediaWiki:Createplate-ContestEntry&redlink=1";
-//header("Location: $url");
 echo $username;
-echo $url;
 ?>
 </body>
 </html>
