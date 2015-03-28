fe02ce25f99b01f0f1ab0b5f7061bbc4126e7347
George Barnick <georgebarnick@gmail.com>
2015-03-27 23:32:26 -0400

    New file for Bricki Super Heroes contest
    JS on Bricki is broken because of cache and can't be updated, so this
    PHP is the only immediate alternative

---
+++ shcontest.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: http://en.brickimedia.org/index.php?title=Brickipedia:Pick_the_Hero_contest/entries/" . $_COOKIE["sharedUserName"] . "&action=edit&preload=MediaWiki:Createplate-ContestEntry&redlink=1");
+die();
+?>
