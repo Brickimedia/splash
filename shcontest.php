6d77883b514a2cfa1f3e97966dcc74542a19ea1b
George Barnick <georgebarnick@gmail.com>
2015-03-27 23:39:15 -0400

    Update

--- shcontest.php
+++ shcontest.php
@@ -1,3 +1,5 @@
 <?php
-header("Location: http://en.brickimedia.org/index.php?title=Brickipedia:Pick_the_Hero_contest/entries/" . $_COOKIE["sharedUserName"] . "&action=edit&preload=MediaWiki:Createplate-ContestEntry&redlink=1");
+$username = $_COOKIE["sharedUserName"];
+$url = "http://en.brickimedia.org/index.php?title=Brickipedia:Pick_the_Hero_contest/entries/" . $_COOKIE["sharedUserName"] . "&action=edit&preload=MediaWiki:Createplate-ContestEntry&redlink=1";
+header("Location: " .$url);
 ?>
